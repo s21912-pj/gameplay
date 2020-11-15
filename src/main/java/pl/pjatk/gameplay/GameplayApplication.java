0a3d4a11eecbc905bf90249eb672b091f33afd71
Karol Kuchnio <kuchniokarol@gmail.com>
2020-11-15 18:52:48 +0100

    [small fix]

--- src/main/java/pl/pjatk/gameplay/GameplayApplication.java
+++ src/main/java/pl/pjatk/gameplay/GameplayApplication.java
@@ -3,7 +3,7 @@ package pl.pjatk.gameplay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"pl.pjatk.gameplay.*"})
+@SpringBootApplication
 public class GameplayApplication {
 
 	public static void main(String[] args) {
